41d0a4450250abbeafe7504e4f566fdede6e6520
Sander Paniago <sanderppaniago@gmail.com>
2022-05-11 15:31:16 -0400

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,37 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useRef, useState } from 'react'
+import {BrowserQRCodeReader} from '@zxing/browser'
+
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  const [data, setData] = useState('Not found')
+
+  useEffect(() => {
+    (async () => {
+      if (videoRef.current) {
+        const videoStream = await navigator.mediaDevices.getUserMedia({ video: true })
+
+        videoRef.current.srcObject = videoStream
+
+        const codeReader = new BrowserQRCodeReader()
+        
+        await codeReader.decodeFromVideoElement(videoRef.current, (value, error, controls) => {
+          console.log(value)
+          if (!value?.getText()) {
+            return
+          }
+          console.log(value)
+          setData(value?.getText())
+          controls.stop()
+        })
+      }
+    })()
+  }, [])
   return (
     <div className={styles.container}>
       <Head>
@@ -17,40 +45,8 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <video autoPlay ref={videoRef} /> 
+        <p>{data}</p>
       </main>
 
       <footer className={styles.footer}>
